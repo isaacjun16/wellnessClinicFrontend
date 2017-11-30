d2c439736757bb129bbb31d4a6a9f805560fdca1
Isaac Manuel <isaacjun16@gmail.com>
2017-11-29 23:00:30 -0600

    Correccion de assets en servicios

--- src/app/services/bservices/bservices.service.ts
+++ src/app/services/bservices/bservices.service.ts
@@ -7,32 +7,32 @@ export class BservicesService {
 
   getBusinessServices() {
     return [{
-      image: "/assets/img/service1.png",
+      image: "assets/img/service1.png",
       description: "",
       title: "Donec elitu commodo"
     },
     {
-      image: "/assets/img/service2.png",
+      image: "assets/img/service2.png",
       description: "",
       title: "Ipsum samsi nayakio"
     },
     {
-      image: "/assets/img/service3.png",
+      image: "assets/img/service3.png",
       description: "",
       title: "Aliguiam Maecenas"
     },
     {
-      image: "/assets/img/service1.png",
+      image: "assets/img/service1.png",
       description: "",
       title: "Donec elitu commodo"
     },
     {
-      image: "/assets/img/service2.png",
+      image: "assets/img/service2.png",
       description: "",
       title: "Ipsum samsi nayakio"
     },
     {
-      image: "/assets/img/service3.png",
+      image: "assets/img/service3.png",
       description: "",
       title: "Aliguiam Maecenas"
     }];
