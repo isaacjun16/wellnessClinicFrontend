76981c1752bde349aa17bc59b9d266ae7d770089
Isaac Manuel <isaacjun16@gmail.com>
2017-11-29 23:07:19 -0600

    Correccion de assets en servicios

--- src/app/services/photos/photos.service.ts
+++ src/app/services/photos/photos.service.ts
@@ -7,12 +7,12 @@ export class PhotosService {
 
   getInstagramPhotos(){
     return [
-      "assets/img/instagramPhoto1.png",
-      "assets/img/instagramPhoto2.png",
-      "assets/img/instagramPhoto3.png",
-      "assets/img/instagramPhoto4.png",
-      "assets/img/instagramPhoto5.png",
-      "assets/img/instagramPhoto2.png"
+      "./assets/img/instagramPhoto1.png",
+      "./assets/img/instagramPhoto2.png",
+      "./assets/img/instagramPhoto3.png",
+      "./assets/img/instagramPhoto4.png",
+      "./assets/img/instagramPhoto5.png",
+      "./assets/img/instagramPhoto2.png"
     ]
   }
 }
